481cf72e8754f018e0a191cef889286a53c0e05d
MetSunSawedOfWire <132921342+MetSunSawedOfWire@users.noreply.github.com>
2026-06-26 12:53:25 +0300

    Add files via upload

---
+++ db.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Ошибка: не удалось загрузить .env файл. Убедись, что он в корне проекта")
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
+		host, user, password, dbName, port)
+
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Ошибка подключения к базе данных: %v", err)
+	}
+
+	DB = database
+	log.Println("Успешное подключение к базе данных")
+}
